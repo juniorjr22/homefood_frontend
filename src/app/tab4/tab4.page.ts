2621774f48460c90ca1a561ea4ee5d075f426812
edirjr <edirgomescjunior@gmail.com>
2022-07-22 19:56:29 -0300

    Foi criado a tela de seleção de ingredientes

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss']
+})
+
+export class Tab4Page {
+
+  
+
+  listIngredients= [
+
+    {id: 1, name: "Arroz"},
+    {id: 2, name: "Feijão"},
+    {id: 3, name: "Macarrão"},
+    {id: 4, name: "Batata"},
+    {id: 5, name: "Azeite"},
+    {id: 6, name: "Pão de Forma"},
+    {id: 7, name: "Alcatra"},
+    {id: 8, name: "Maminha"},
+    {id: 9, name: "Frango"}
+    
+  ]
+  
+  allIngredients:any; 
+  queryText: string;
+
+  constructor() {
+    this.queryText = '';
+    
+    this.allIngredients = this.listIngredients;
+
+  }
+
+  filterIngredient(ingredient: any){
+
+    let val = ingredient.target.value;
+    if (val && val.trim() != ''){
+      this.listIngredients = _.values(this.allIngredients);
+      this.listIngredients = this.listIngredients.filter((ingredient) => {
+        return (ingredient.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+        
+    } else {
+      this.listIngredients = this.allIngredients;
+    }
+
+
+  }
+  
+
+}
